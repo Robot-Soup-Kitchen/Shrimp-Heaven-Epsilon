cad3171a17a344f58f6c9d54cafce98e9c096683
Quinn Pham <quinnlp12@gmail.com>
2020-01-18 17:32:36 -0700

    user done

--- app/src/main/java/com/example/catnmouse/User.java
+++ app/src/main/java/com/example/catnmouse/User.java
@@ -3,9 +3,45 @@ package com.example.catnmouse;
 public class User {
     String username;
     String password;
+    double lon;
+    double lat;
+    User partner;
+    boolean isCat;
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public User getPartner() {
+        return partner;
+    }
+
+    public void setPartner(User partner) {
+        this.partner = partner;
+    }
+
+    public boolean isCat() {
+        return isCat;
+    }
+
+    public void setCat(boolean cat) {
+        isCat = cat;
     }
 
     public String getUsername() {
@@ -24,4 +60,3 @@ public class User {
         this.password = password;
     }
 }
-
